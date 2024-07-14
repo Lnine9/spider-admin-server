a4af931e18d322fdfe1047c81e1f04f5a9f1f367
KomY777 <2930738331@qq.com>
2024-07-14 15:36:52 +0800

    fix：加入连接池，防止手动操作连接失效

--- model/base.py
+++ model/base.py
@@ -1,7 +1,15 @@
 from peewee import *
+from playhouse.pool import PooledMySQLDatabase
+
 from setting import MYSQL
 
-db = MySQLDatabase(MYSQL['DB'], host=MYSQL['HOST'], port=MYSQL['PORT'], user=MYSQL['USER'], password=MYSQL['PASSWORD'])
+db = PooledMySQLDatabase(MYSQL['DB'],
+                         max_connections=10,
+                         stale_timeout=300,
+                         host=MYSQL['HOST'],
+                         port=MYSQL['PORT'],
+                         user=MYSQL['USER'],
+                         password=MYSQL['PASSWORD'])
 db.connect()
 
 class BaseModel(Model):
