60f1dceb094caa085f1a3f6086bb97a6fefcfe7f
KomY777 <2930738331@qq.com>
2024-08-29 15:23:53 +0800

    fix：加入连接池，防止手动操作连接失效

--- model/base.py
+++ model/base.py
@@ -1,16 +1,27 @@
+import time
+
 from peewee import *
 from playhouse.pool import PooledMySQLDatabase
+from playhouse.shortcuts import ReconnectMixin
 
 from setting import MYSQL
 
-db = PooledMySQLDatabase(MYSQL['DB'],
-                         max_connections=10,
-                         stale_timeout=300,
-                         host=MYSQL['HOST'],
-                         port=MYSQL['PORT'],
-                         user=MYSQL['USER'],
-                         password=MYSQL['PASSWORD'])
-db.connect()
+class ReconnectPooledMySQLDatabase(ReconnectMixin, PooledMySQLDatabase):
+    _instance = None
+
+    @classmethod
+    def get_db_instance(cls):
+        if not cls._instance:
+            cls._instance = cls(MYSQL['DB'],
+                                max_connections=10,
+                                stale_timeout=300,
+                                host=MYSQL['HOST'],
+                                port=MYSQL['PORT'],
+                                user=MYSQL['USER'],
+                                password=MYSQL['PASSWORD'])
+        return cls._instance
+# 数据库实例
+db = ReconnectPooledMySQLDatabase.get_db_instance()
 
 class BaseModel(Model):
     class Meta:
